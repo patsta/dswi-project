1b1947026a1e74cd90eed301f0a76d114a5c3f4e
Sander Van den Bogaert <r0632225@student.thomasmore.be>
2021-12-11 14:08:35 +0100

    libraries and data import

--- script.R
+++ script.R
@@ -3,7 +3,26 @@
 # Date: 11.12.2021
 # Description: Project paper
 ################################################################################
-library(readr)
+# 1. - Packages
+# 2. - Dataset
+# 3. - Tidy up!
+# 4. - Visualization
 
+
+# 1. - Packages-----------------------------------------------------------------
+# Packages used in this research (You may need to install the packages 
+# before loading them)
+library(readr)
+library(scatterplot3d) 
+library(readxl)
+
+library(rgl)
+library(car) 
+
+library(MASS)
+library(ggplot2)
+
+# 2. - Dataset------------------------------------------------------------------
 # import data
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 initial_data <- read.csv('BodyFat.csv')
