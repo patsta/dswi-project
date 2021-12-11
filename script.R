14c7ca932d662a261a1f40c5eee21f3b2f3a37dd
patsta <>
2021-12-11 13:53:24 +0100

    setup R-project

---
+++ script.R
@@ -0,0 +1,9 @@
+################################################################################
+# Authors: Martin Clément, Patrice Stampfli, Sander van den Bogaert
+# Date: 11.12.2021
+# Description: Project paper
+################################################################################
+library(readr)
+
+# import data
+initial_data <- read.csv('BodyFat.csv')
